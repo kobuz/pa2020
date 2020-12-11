089fcd0b318fddfcf1128167fb79aefba57ab962
Marcin Sokół <kobbuz@gmail.com>
2020-12-11 19:36:21 +0100

    Improve performance of mal

--- r4/B-mal.cpp
+++ r4/B-mal.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <set>
-#include <map>
 #include <algorithm>
 using namespace std;
 
@@ -8,13 +6,6 @@ using namespace std;
 int n, m;
 long long p;
 
-
-void print(int round, long long *ptr) {
-    cout << "round " << round << endl;
-    for(int lvl = 0; lvl < m; ++lvl) {
-        cout << lvl << " : " << ptr[lvl] << endl;
-    }
-}
 
 int main() {
     std::ios::sync_with_stdio(false);
@@ -24,31 +15,27 @@ int main() {
     for(int lvl = 0; lvl < m; ++lvl) {
         prev[lvl] = m - lvl;
     }
-    // print(0, prev);
+    long long sums[m], woos[m];
     for(int i = 1; i < n; ++i) {
-        cout << i << endl;
         for(int lvl = 0; lvl < m; ++lvl) { // reset 
-            next[lvl] = 0;
+            next[lvl] = sums[lvl] = woos[lvl] = 0;
         }
-        for(int lvl = 0; lvl < m; ++lvl) {
-            int h_count = m - lvl;
-            long long base;
-            // cout << "lvl=" << lvl << endl;
-            for(int prev_lvl = 0; prev_lvl < lvl; ++prev_lvl) { // prev started at lower level
-                base = ((prev[prev_lvl] - prev[m - prev_lvl - 1] + p) % p);
-                // cout << "I prev_lvl=" << prev_lvl << " part=" << part << " h_count=" << h_count << endl;
-                next[lvl] += base * h_count;
+        sums[0] = prev[0];
+        woos[m - 1] = prev[m - 1];
+        for(int lvl = 1; lvl < m; ++lvl) { // prefix sums and the other thing for speedup
+            sums[lvl] = sums[lvl - 1] + prev[lvl];
+            sums[lvl] %= p;
+            woos[m - lvl - 1] = woos[m - lvl] + ((lvl + 1) * prev[m - lvl - 1]) % p;
+            woos[m - lvl - 1] %= p;
+        }
+        for(int lvl = 0; lvl < m; ++lvl) { // main calculations
+            if(lvl > 0) {
+                next[lvl] = p + (sums[lvl - 1] - (sums[m - 1] - sums[m - lvl - 1])) * (m - lvl);
                 next[lvl] %= p;
             }
-            for(int prev_lvl = lvl; prev_lvl < m; ++prev_lvl) { // prev started at the same level or higher
-                base = prev[prev_lvl];
-                // cout << "II prev_lvl=" << prev_lvl << " part=" << part << " h_count=" << h_count << endl;
-                next[lvl] += base * h_count;
-                next[lvl] %= p;
-                h_count -= 1;
-            }
+            next[lvl] += woos[lvl];
+            next[lvl] %= p;
         }
-        // print(i, next);
         long long *tmp = prev; // switch pointers
         prev = next;
         next = tmp;
