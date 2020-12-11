ccf144be9fc5a7bd9ae8f002564efe2ab58fecee
Marcin Sokół <kobbuz@gmail.com>
2020-12-11 22:16:11 +0100

    Speed up wyc

--- r4/C-wyc.cpp
+++ r4/C-wyc.cpp
@@ -1,21 +1,25 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <map>
 #include <algorithm>
 using namespace std;
 
 typedef pair<int, int> Coord;
-typedef pair<int, int> Result;
+
+int n, m, k;
+
+
+int pack(int x, int y) {
+    return x + m * y;
+}
 
 int main() {
     std::ios::sync_with_stdio(false);
-    int n, m, k;
     cin >> n >> m >> k;
     vector<string > world_map;
     string input;
-    map<Coord, Result> results;
-    results[{0, 0}] = {0, 0};
+    vector<int> ups(m * n, -1), downs(m * n, -1);
+    ups[0] = downs[0] = 0;
     for(int i = 0; i < n; ++i) { // map
         cin >> input;
         world_map.push_back(input);
@@ -27,23 +31,22 @@ int main() {
     while(!q.empty()) {
         Coord node = q.front();
         q.pop();
-        int x = get<0>(node), y = get<1>(node);
-        Result res = results[{x, y}];
-        int ups = get<0>(res), downs = get<1>(res);
+        int x = node.first, y = node.second;
+        int sup = ups[pack(x, y)], sdown = downs[pack(x, y)];
         for(auto delta: changes) {
-            int dx = get<0>(delta), dy = get<1>(delta);
+            int dx = delta.first, dy = delta.second;
             int xx = x + dx, yy = y + dy;
-            if(xx < 0 || xx >= m || yy < 0 || yy >= n || world_map[yy][xx] == 'X' || results.find({xx, yy}) != results.end() ) {
+            if(xx < 0 || xx >= m || yy < 0 || yy >= n || world_map[yy][xx] == 'X' || ups[pack(xx, yy)] != -1 ) {
                 continue;
             }
-            int nups = ups + ((dx + dy) > 0 ? 1 : 0), ndowns = downs + ((dx + dy) < 0 ? 1 : 0);
-            results[{xx, yy}] = {nups, ndowns};
+            int nups = sup + ((dx + dy) > 0 ? 1 : 0), ndowns = sdown + ((dx + dy) < 0 ? 1 : 0);
+            ups[pack(xx, yy)] = nups;
+            downs[pack(xx, yy)] = ndowns;
             q.push({xx, yy});
         }
     }
 
-    Result sol = results[{m - 1, n - 1}];
-    int up = get<0>(sol), down = get<1>(sol);
+    int up = ups[pack(m - 1, n - 1)], down = downs[pack(m - 1, n - 1)];
     int a, b;
     vector<int> times;
     for(int i = 0; i < k; ++i) { // travellers
